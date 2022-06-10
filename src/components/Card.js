8f64d55301afa85492fa69e9fb76665158d73f3e
yaofeng <navy_blue@163.com>
2022-06-10 11:42:15 +0800

    finish all

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,44 @@
+import { useState } from 'react';
 import "./card.css";
-import mobileDivider from '../images/pattern-divider-mobile.svg';
 import dice from '../images/icon-dice.svg';
 
 const Card = () => {
+    const originId = 117;
+    const originSentence = "It is easy to sit up and take notice, what's difficult is getting up and taking action.";
+    const [id, setId] = useState(originId);
+    const [sentence, setSentence] = useState(originSentence);
+
+    let allowClick = true;
+
+
+    const handleClick = () => {
+        console.log(allowClick);
+        if (allowClick) {
+            allowClick = false;
+            fetch("https://api.adviceslip.com/advice").then(res => res.json()).then(result => {
+                const slip = result.slip;
+                const id = slip.id;
+                const advice = slip.advice;
+                setId(id);
+                setSentence(advice);
+                allowClick = true;
+            });
+        }
+    };
 
     return (
         <div className="card">
             <header className="header">
-                <p className="title">
-                    <span className="title__advice">ADVICE</span>
-                    <span className="title__pound-sign">#</span>
-                    <span className="title__number">117</span>
+                <p className="ident">
+                    <span className="ident__advice">ADVICE</span>
+                    <span className="ident__pound-sign">#</span>
+                    <span className="ident__number">{id}</span>
                 </p>
             </header>
             <main className="main">
-                <h1 className="sentence">“It is easy to sit up and take notice, what's difficult is getting up and taking action.”</h1>
-                <img className="divider" src={mobileDivider} alt="divider"></img>
-                <div className="button">
+                <h1 className="sentence"><q className="quotes">{sentence}</q></h1>
+                <div className="divider"></div>
+                <div className="button" onClick={handleClick}>
                     <img className="dice" src={dice} alt="dice"></img>
                 </div>
             </main>
